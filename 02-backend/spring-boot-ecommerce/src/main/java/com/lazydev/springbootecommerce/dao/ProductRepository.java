dc5d9b41aa74ae32bd1cceec73536ef860cd363e
XIVIXMMI <lethaikhoinguyen@gmail.com>
2024-01-27 21:01:01 +0700

    display product

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lazydev/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lazydev/springbootecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.lazydev.springbootecommerce.dao;
 
 import com.lazydev.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") //Origin is protocol + hostname + port
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
