ec204589ac36a628db18d51ad350f4a3b6439340
XIVIXMMI <lethaikhoinguyen@gmail.com>
2024-03-02 23:00:40 +0700

    feat: Member Access - Protected Routes

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc:{
-        clientId: '0oafh0w64tdM0EJ2o5d7', // public identifier
-        issuer: 'https://dev-72077753.okta.com/oauth2/default', //issuer of tokens, URL when authorizing with okta authorization server
+        clientId: '0oafhroginNOCoEdZ5d7', // public identifier
+        issuer: 'https://dev-60254125.okta.com/oauth2/default', //issuer of tokens, URL when authorizing with okta authorization server
         redirectUri: 'http://localhost:4200/login/callback', // Once user logs in send them here
         scopes: ['openid', 'profile', 'email'] // Scropes provide access to infomation about a user 
         /*
